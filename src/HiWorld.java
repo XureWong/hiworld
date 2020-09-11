1f52bd7cc505d77da5814c0c73a66f1dc052d7c5
18252034271 <wangxu0>
2020-09-11 17:43:24 +0800

    test 修改后提交

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hi World 这里是新建项目上传到git上");
+        System.out.println("Hi World 这里是新建项目上传到git上 二次修改提交");
     }
 }
